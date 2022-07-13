94b0c544c48cf07515d80225ef868a500b9caa11
XFizzer <giantbacontv@gmail.com>
2022-07-13 14:34:09 -0700

    Add files via upload

--- Games/Boilerplate/index.js
+++ Games/Boilerplate/index.js
@@ -1,1 +1,69 @@
 const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+class Player {
+  constructor(x, y, raduis, color) {
+    this.x = x
+    this.y = y
+    this.raduis = raduis
+    this.color = color
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.raduis, 0, Math.PI * 2, false);
+    c.fillStyle = this.color
+    c.fill()
+  }
+}
+
+class Projectile {
+  constructor(x, y, raduis, color, velocity) {
+    this.x = x
+    this.y = y
+    this.raduis = raduis
+    this.color = color
+    this.velocity = velocity
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.raduis, 0, Math.PI * 2, false);
+    c.fillStyle = this.color
+    c.fill()
+  }
+
+  update() {
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+const player = new Player(x, y, 30, 'blue')
+player.draw()
+const projectile = new Projectile(
+  canvas.width / 2,
+  canvas.height / 2,
+  5,
+  'red', 
+  {
+    x: 1,
+    y: 1
+  }
+)
+
+function animate() {
+  requestAnimationFrame(animate)
+  projectile.draw()
+  projectile.update()
+}
+
+addEventListener('click', (event) => {
+})
+
+animate()
